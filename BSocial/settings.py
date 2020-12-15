303502ab6f4fc4c638f8d00d1e8503eeb3efb641
tasnidmahin <tasnidmahin@gmail.com>
2020-12-15 19:18:28 +0600

    RESTAPI setup

--- BSocial/settings.py
+++ BSocial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'BSocial.wsgi.application'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Database
